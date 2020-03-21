f561e8a2290608dbf2e878e4fe9d87e213f33a79
Jean Osco <willjean29@gmail.com>
2020-03-21 16:00:37 -0500

    change

--- models/Conexion.php
+++ models/Conexion.php
@@ -3,18 +3,18 @@
         static public function conectar(){
 
             //Get Heroku ClearDB connection information
-            // $cleardb_url      = parse_url(getenv("CLEARDB_DATABASE_URL"));
-            // $cleardb_server   = $cleardb_url["host"];
-            // $cleardb_username = $cleardb_url["user"];
-            // $cleardb_password = $cleardb_url["pass"];
-            // $cleardb_db       = substr($cleardb_url["path"],1);
+            $cleardb_url      = parse_url(getenv("CLEARDB_DATABASE_URL"));
+            $cleardb_server   = $cleardb_url["host"];
+            $cleardb_username = $cleardb_url["user"];
+            $cleardb_password = $cleardb_url["pass"];
+            $cleardb_db       = substr($cleardb_url["path"],1);
 
             // server local
             // $cleardb_url      = parse_url(getenv("CLEARDB_DATABASE_URL"));
-            $cleardb_server   = 'localhost';
-            $cleardb_username = 'root';
-            $cleardb_password = '';
-            $cleardb_db       = 'curso-php';
+            // $cleardb_server   = 'localhost';
+            // $cleardb_username = 'root';
+            // $cleardb_password = '';
+            // $cleardb_db       = 'curso-php';
             try {
                 $conn = new PDO("mysql:host=$cleardb_server;dbname=$cleardb_db",
                 $cleardb_username,$cleardb_password);
